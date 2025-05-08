2288f2be75ad0343d053e12912059f364b3ddf76
Artem Lira <artemlira84@gmail.com>
2025-05-08 16:15:34 +0300

    added a new section "Daycare gallery" under task PBS-343

--- page-daycare-partnerships.php
+++ page-daycare-partnerships.php
@@ -306,6 +306,45 @@ $green_yellow_line_title = get_field('daycare_page_green_yellow_line_title');
   </div>
 </section>
 
+<section class="daycare-gallery wow fadeIn">
+  <div class="container">
+    <div class="titleBlock">
+      <h2 class="title"><?php echo get_field('daycare_page_gallery_title') ?></h2>
+      <div class="description"><?php echo get_field('daycare_page_gallery_description') ?></div>
+    </div>
+    <?php if (have_rows('daycare_page_gallery_content')): ?>
+      <div class="contentBlock">
+        <?php while (have_rows('daycare_page_gallery_content')): the_row();
+          $image = get_sub_field('image');
+          $item_id = 'content-item-' . get_row_index();
+          ?>
+          <div class="contentItem">
+            <a href="javascript:;" data-src="#<?php echo $item_id; ?>" data-fancybox>
+              <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>">
+            </a>
+            <div id="<?php echo $item_id; ?>" style="display: none;">
+              <div class="fancybox-custom-container">
+                <button class="fancybox-custom-close">
+                  <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="M19.5 20.7156L10.4895 29.7256C10.3289 29.8865 10.1318 29.9724 9.89826 29.9833C9.66496 29.9944 9.457 29.9085 9.27437 29.7256C9.09146 29.543 9 29.3405 9 29.1181C9 28.8956 9.09146 28.6931 9.27437 28.5105L18.2844 19.5L9.27437 10.4895C9.1135 10.3289 9.02762 10.1318 9.01675 9.89826C9.00558 9.66496 9.09146 9.457 9.27437 9.27437C9.457 9.09146 9.65952 9 9.88194 9C10.1044 9 10.3069 9.09146 10.4895 9.27437L19.5 18.2844L28.5105 9.27437C28.6711 9.1135 28.8682 9.02762 29.1017 9.01674C29.335 9.00558 29.543 9.09146 29.7256 9.27437C29.9085 9.457 30 9.65952 30 9.88194C30 10.1044 29.9085 10.3069 29.7256 10.4895L20.7156 19.5L29.7256 28.5105C29.8865 28.6711 29.9724 28.8682 29.9833 29.1017C29.9944 29.335 29.9085 29.543 29.7256 29.7256C29.543 29.9085 29.3405 30 29.1181 30C28.8956 30 28.6931 29.9085 28.5105 29.7256L19.5 20.7156Z"
+                        fill="#363636"/>
+                  </svg>
+                </button>
+                <div class="fancybox-custom-content">
+                  <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>">
+                </div>
+              </div>
+            </div>
+          </div>
+        <?php endwhile ?>
+      </div>
+    <?php endif; ?>
+    <div class="text"><?php echo get_field('daycare_page_gallery_text'); ?></div>
+    <div class="slogan"><?php echo get_field('daycare_page_gallery_slogan'); ?></div>
+  </div>
+</section>
+
 
 <?php get_template_part('template-parts/instagram-feed-5'); ?>
 <?php get_template_part('template-parts/subscribe-form'); ?>
